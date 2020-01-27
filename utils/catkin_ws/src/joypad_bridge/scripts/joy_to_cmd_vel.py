9d6dffc9e26d8b3170ea483016480659942008ed
Federica Di Lauro <federicadilauro1998@gmail.com>
2020-01-27 17:39:06 +0100

    clean repo

--- utils/catkin_ws/src/joypad_bridge/scripts/joy_to_cmd_vel.py
+++ utils/catkin_ws/src/joypad_bridge/scripts/joy_to_cmd_vel.py
@@ -3,29 +3,17 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
-# Author: Andrew Dai
-# This ROS Node converts Joystick inputs from the joy node
-# into cmd_vel
-
-# Receives joystick messages (subscribed to Joy topic)
-# then converts the joysick inputs into Twist commands
-# axis 1 aka left stick vertical controls linear speed
-# axis 0 aka left stick horizonal controls angular speed
 def callback(data):
     twist = Twist()
     twist.linear.x = data.axes[2]
     twist.angular.z = data.axes[0]
     pub.publish(twist)
 
-#Intializes everything
 def start():
-    # publishing to "turtle1/cmd_vel" to control turtle1
     global pub
     rospy.init_node('joy_to_cmd_vel')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    # subscribed to joystick inputs on topic "joy"
     rospy.Subscriber("joy", Joy, callback)
-    # starts the node
     rospy.spin()
 
 if __name__ == '__main__':
