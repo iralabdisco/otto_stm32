326db3e78ffff1a064ea7b50d4d2903eebdf2ba3
Federica Di Lauro <federicadilauro1998@gmail.com>
2020-02-06 09:23:50 +0100

    clean utils folder

--- utils/catkin_ws/build/joypad_bridge/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fdila/Projects/otto/utils/catkin_ws/src/joypad_bridge
-# Build directory: /home/fdila/Projects/otto/utils/catkin_ws/build/joypad_bridge
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
